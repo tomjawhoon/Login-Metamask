4c8bf63475e96ac502303e4922f047d42dccd9f7
naratorn_152@hotmail.com <naratorn_152@hotmail.com>
2022-02-09 18:29:50 +0700

    edit

--- src/main.tsx
+++ src/main.tsx
@@ -2,24 +2,25 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './App.css'
 import 'antd/dist/antd.css';
-import App from './App'
+// import App from './App'
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Connectmetamask from "./pages/connectmetamask"
 import Switchaccount from "./pages/switchaccount"
 import Switchnetwork from "./pages/switchnetwork"
 import Mobileconnect from "./pages/mobileconnect"
-
+import Homepage from "./pages/homepage"
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<App />} />
+        <Route path="/" element={<Homepage />} />
         <Route path="/connectmetamask" element={<Connectmetamask />} />
         <Route path="/switchaccount" element={<Switchaccount />} />
         <Route path="/switchnetwork" element={<Switchnetwork />} />
         <Route path="/mobileconnect" element={<Mobileconnect />} />
       </Routes>
+      <Homepage />
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
